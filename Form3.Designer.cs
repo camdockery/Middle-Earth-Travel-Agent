e9aeab5cd7b46d0af2952f61d6a3e4a7120ab326
camdockery <cdockery102@gmail.com>
2025-04-12 10:58:01 -0400

    [File] ensured .json was transferrable between systems

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -29,31 +29,45 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            checkedListBox1 = new CheckedListBox();
             SuspendLayout();
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(364, 5);
+            label1.BackColor = SystemColors.Window;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(2, -1);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Size = new Size(800, 131);
+            label1.TabIndex = 2;
+            label1.Text = "Booking";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Items.AddRange(new object[] { "Horse?", "Great Eagle?" });
+            checkedListBox1.Location = new Point(594, 149);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(120, 40);
+            checkedListBox1.TabIndex = 3;
             // 
             // Form3
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(checkedListBox1);
             Controls.Add(label1);
             Name = "Form3";
             Text = "Form3";
+            Load += Form3_Load;
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Label label1;
+        private CheckedListBox checkedListBox1;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Middle_Earth_Travel_Agent
 {
@@ -15,6 +17,28 @@ namespace Middle_Earth_Travel_Agent
         public Form3()
         {
             InitializeComponent();
+            this.BackColor = ColorTranslator.FromHtml("#f5eece");
+        }
+
+        PrivateFontCollection customFonts = new PrivateFontCollection();
+
+        private void LoadCustomFont()
+        {
+            string fontPath = Path.Combine(Application.StartupPath, "Middleearth-ao6m.ttf");
+            customFonts.AddFontFile(fontPath);
+
+            Font customFont = new Font(customFonts.Families[0], 25F, FontStyle.Bold);
+            Font customFont2 = new Font(customFonts.Families[0], 15F, FontStyle.Regular);
+            Font customFont3 = new Font(customFonts.Families[0], 10F, FontStyle.Regular);
+            label1.Font = customFont;
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            LoadCustomFont();
+            label1.ForeColor = ColorTranslator.FromHtml("#b87316");
+            label1.BackColor = ColorTranslator.FromHtml("#f5eece");
         }
     }
 }
